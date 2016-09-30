63b265b8027d041038de2e556986354348fb1a9e
Alin Jerpelea <alin.jerpelea@sonymobile.com>
2016-09-30 12:37:33 +0200

    inital configuration
    initial configuration for Sony Xperia X Performance (dora)
    
    Signed-off-by: Alin Jerpelea <alin.jerpelea@sonymobile.com>

---
+++ device.mk
@@ -0,0 +1,74 @@
+# Copyright 2014 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+DEVICE_PACKAGE_OVERLAYS += \
+    device/sony/dora/overlay
+
+PRODUCT_COPY_FILES := \
+    device/sony/dora/rootdir/system/etc/audio_policy.conf:system/etc/audio_policy.conf \
+    device/sony/dora/rootdir/system/etc/BCM4359C0.hcd:system/etc/firmware/BCM43xx.hcd \
+    device/sony/dora/rootdir/system/etc/wifi/bcmdhd.cal:system/etc/wifi/bcmdhd.cal \
+    device/sony/dora/rootdir/system/etc/sensors/sensor_def_qcomdev.conf:system/etc/sensors/sensor_def_qcomdev.conf \
+    device/sony/dora/rootdir/system/etc/thermanager.xml:system/etc/thermanager.xml \
+    device/sony/dora/rootdir/system/etc/libnfc-brcm.conf:system/etc/libnfc-brcm.conf \
+    device/sony/dora/rootdir/system/etc/libnfc-nxp.conf:system/etc/libnfc-nxp.conf \
+    device/sony/dora/rootdir/system/etc/mixer_paths.xml:system/etc/mixer_paths.xml
+
+# Device Specific Permissions
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml \
+    frameworks/native/data/etc/android.hardware.fingerprint.xml:system/etc/permissions/android.hardware.fingerprint.xml \
+    frameworks/native/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml \
+    frameworks/native/data/etc/android.hardware.sensor.barometer.xml:system/etc/permissions/android.hardware.sensor.barometer.xml \
+    frameworks/native/data/etc/android.hardware.sensor.gyroscope.xml:system/etc/permissions/android.hardware.sensor.gyroscope.xml
+
+# Device Init
+PRODUCT_PACKAGES += \
+    init.recovery.dora \
+    init.dora \
+    ueventd.dora
+
+# Lights
+PRODUCT_PACKAGES += \
+    lights.dora
+
+# Simple PowerHAL
+PRODUCT_PACKAGES += \
+    power.dora
+
+# Fingerprint HAL
+PRODUCT_PACKAGES += \
+    fingerprintd \
+    fingerprint.dora
+
+# NFC config
+PRODUCT_PACKAGES += \
+    nfc_nci.dora
+
+# Telephony Packages (AOSP)
+PRODUCT_PACKAGES += \
+    InCallUI \
+    Stk
+
+PRODUCT_AAPT_CONFIG := normal
+PRODUCT_AAPT_PREBUILT_DPI := xxhdpi xhdpi hdpi
+PRODUCT_AAPT_PREF_CONFIG := xxhdpi
+
+PRODUCT_PROPERTY_OVERRIDES := \
+    ro.sf.lcd_density=480 \
+    ro.usb.pid_suffix=1E1
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, device/sony/tone/platform.mk)
+$(call inherit-product, vendor/sony/tone-dora/dora-vendor.mk)
